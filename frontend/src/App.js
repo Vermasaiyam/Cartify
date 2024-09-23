4968e61a4e9556e0532f99c2b5441df81a6605be
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-23 21:51:42 +0530

    assets and structure of home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 }
 
