9f44237f2fd35c1cf4471c513d3924151261e034
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-24 21:01:43 +0530

    update user role

--- frontend/src/pages/AllUsers.jsx
+++ frontend/src/pages/AllUsers.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import SummaryApi from '../common';
 import { toast } from 'react-toastify';
 import { MdModeEdit } from "react-icons/md";
+import moment from 'moment'
+import ChangeUserRole from '../components/ChangeUserRole';
 
 const AllUsers = () => {
 
@@ -31,33 +33,33 @@ const AllUsers = () => {
 
     return (
         <div className='bg-white pb-4'>
-            <table className='w-full userTable'>
+            <table className='w-full userTable' >
                 <thead>
                     <tr className='bg-black text-white'>
-                        <th>Sr.</th>
+                        <th className='py-1'>Sr.</th>
                         <th>Name</th>
                         <th>Email</th>
                         <th>Role</th>
-                        {/* <th>Created Date</th> */}
-                        <th>Action</th>
+                        <th>Created Date</th>
+                        <th>Edit</th>
                     </tr>
                 </thead>
                 <tbody className=''>
                     {
                         allUser.map((el, index) => {
                             return (
-                                <tr>
-                                    <td>{index + 1}</td>
+                                <tr >
+                                    <td className='py-3'>{index + 1}</td>
                                     <td>{el?.name}</td>
                                     <td>{el?.email}</td>
                                     <td>{el?.role}</td>
-                                    {/* <td>{moment(el?.createdAt).format('LL')}</td> */}
+                                    <td>{moment(el?.createdAt).format('LL')}</td>
                                     <td>
                                         <button className='bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green-500 hover:text-white'
-                                            // onClick={() => {
-                                                // setUpdateUserDetails(el)
-                                                // setOpenUpdateRole(true)
-                                            // }}
+                                        // onClick={() => {
+                                        // setUpdateUserDetails(el)
+                                        // setOpenUpdateRole(true)
+                                        // }}
                                         >
                                             <MdModeEdit />
                                         </button>
@@ -68,6 +70,20 @@ const AllUsers = () => {
                     }
                 </tbody>
             </table>
+
+            {
+                // openUpdateRole && 
+                (
+                    <ChangeUserRole
+                        // onClose={() => setOpenUpdateRole(false)}
+                        // name={updateUserDetails.name}
+                        // email={updateUserDetails.email}
+                        // role={updateUserDetails.role}
+                        // userId={updateUserDetails._id}
+                        // callFunc={fetchAllUsers}
+                    />
+                )
+            }
         </div>
     )
 }
