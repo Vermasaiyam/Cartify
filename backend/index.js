3b2c5800f4ac3d7984afd5d9f13fa56070b58f14
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-24 13:23:08 +0530

    signup api

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const router = require('./routes');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use('/api', router);
 
 const PORT = 8000 || process.env.PORT;
 
-app.listen(PORT, ()=> {
-    console.log("Server Started.");
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnected to DB")
+        console.log("Server is running "+PORT)
+    })
 })
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const Signup = require('../controllers/signup');
+
+const router = express.Router();
+
+router.post('/signup', Signup);
+
+
+module.exports = router
