aa8619021a6b61c8b45e5bc0dc55abbac81fb568
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-27 15:46:47 +0530

    bugs resolved on product detail page

--- frontend/src/components/CategroyWiseProductDisplay.js
+++ frontend/src/components/CategroyWiseProductDisplay.js
@@ -56,7 +56,7 @@ const CategroyWiseProductDisplay = ({ category, heading }) => {
                     ) : (
                         data.map((product, index) => {
                             return (
-                                <Link to={"product/" + product?._id} className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow cursor-pointer">
+                                <Link to={"/product/"+product?._id} className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow cursor-pointer">
                                     <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center'>
                                         <img src={product.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply' />
                                     </div>
