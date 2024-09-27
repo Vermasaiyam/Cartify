cd15ef8d99197d13c1cba88ed828e04db680c8dc
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-27 22:42:57 +0530

    home page updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -13,17 +13,14 @@ const Home = () => {
       <HorizontalCardProduct category={"airpodes"} heading={"Top Airpodes"} />
       <HorizontalCardProduct category={"watches"} heading={"Popular Watches"} />
 
+      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"} />
+      <VerticalCardProduct category={"televisions"} heading={"Televisions"} />
+      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"} />
       <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"} />
       <VerticalCardProduct category={"mobiles"} heading={"Mobiles"} />
       <VerticalCardProduct category={"Mouse"} heading={"Mouse"} />
       <VerticalCardProduct category={"camera"} heading={"Camera & Photography"} />
       <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"} />
-
-
-      {/* <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"} /> */}
-      {/* <VerticalCardProduct category={"televisions"} heading={"Televisions"} /> */}
-      {/* <VerticalCardProduct category={"trimmers"} heading={"Trimmers"} /> */}
-
 
     </div>
   )
